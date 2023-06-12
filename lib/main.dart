3fe41d7a2de7ba2c4a2e37afbdaae725bd939176
Adnan Farooq <lumia.college.learning.458@gmail.com>
2023-06-12 23:35:54 +0500

    update

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_map/flutter_map.dart';
+import 'package:latlong2/latlong.dart';
 import 'package:states_rebuilder/states_rebuilder.dart';
-
-import 'features/presentations/pages/home_page.dart';
+import 'package:trukk_driver/core/extensions.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+final MapController _mapController = MapController();
+
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
@@ -20,7 +28,24 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const HomePage(),
+      home: Scaffold(
+        appBar: AppBar(
+          title: 'TELLOMERE'.textify(),
+        ),
+        body: FlutterMap(
+          mapController: _mapController,
+          options: MapOptions(
+            center: const LatLng(
+              2,
+              2,
+            ),
+          ),
+          nonRotatedChildren: const [],
+          children: const [
+            // HomePage(),
+          ],
+        ),
+      ),
     );
   }
 }
